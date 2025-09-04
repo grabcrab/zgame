4f3c7f95ae3363ea9ad85444a691a01c3f201254
Victor Golub <35024573+grabcrab@users.noreply.github.com>
2025-09-04 17:05:00 +0300

    Regular

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "1227"
+  #define BUILD_NUMBER "1228"
 #endif
 #ifndef VERSION
-  #define VERSION_STR "v0.1.250809_1227 - 2025-08-09 22:14:40.316370"
+  #define VERSION_STR "v0.1.250904_1228 - 2025-09-04 17:04:33.607424"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.250809_1227"
+  #define VERSION_SHORT "v0.1.250904_1228"
 #endif
